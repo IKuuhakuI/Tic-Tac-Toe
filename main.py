cc5a0915ad7de96d44696e3578fb71ae210c0b38
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2021-06-15 14:59:31 -0300

    Creates the play function

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def main ():
 	print ("TIC TAC TOE")
 	print (game.board)
 
+	print (game.currentTurn)
+	game.play (1, 1)
 main()
--- ticTacToe.py
+++ ticTacToe.py
@@ -1,6 +1,42 @@
 class Game ():
-
 	def __init__ (self):
 		self.board = [[0,0,0],[0,0,0],[0,0,0]] # Empty board
-
 		self.currentTurn = 1 # 1 = X / -1 = O
+
+	def switchTurn (self):
+		if self.currentTurn == 1:
+			self.currentTurn = -1
+
+		else:
+			self.currentTurn = 1
+
+	def checkInput (self, xValue, yValue):
+		minX = minY = 0
+		maxX = maxY = len(self.board)
+		
+		if (maxX > xValue >= minX and maxY > yValue >= minY):
+			if (self.board[xValue][yValue] == 0):
+				return True
+			else:
+				print ("This position has already been chosen")
+
+				return False
+
+		print ("Invalid input!")
+		print ("X and Y values must be between 1 and 3")
+		
+		return False
+
+	def play (self, xInput, yInput):
+
+		xValue = xInput - 1
+		yValue = yInput -1
+
+		isValid = self.checkInput (xValue, yValue)
+
+		if (isValid):
+			self.board[xValue][yValue] = self.currentTurn
+
+			self.switchTurn()
+
+		print (self.board)

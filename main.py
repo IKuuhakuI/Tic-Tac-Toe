cbd1675824570097237903d2fb7c439379ca6603
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2021-06-15 14:43:14 -0300

    Creates the Game class

---
+++ main.py
@@ -0,0 +1,9 @@
+from ticTacToe import Game
+
+def main ():
+	game = Game()
+
+	print ("TIC TAC TOE")
+	print (game.board)
+
+main()
---
+++ ticTacToe.py
@@ -0,0 +1,6 @@
+class Game ():
+
+	def __init__ (self):
+		self.board = [[0,0,0],[0,0,0],[0,0,0]] # Empty board
+
+		self.currentTurn = 1 # 1 = X / -1 = O
